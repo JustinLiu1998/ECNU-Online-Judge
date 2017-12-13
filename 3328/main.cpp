2c14c7782f57a5e0e8cbc7c727f6566c6bd4b4d2
刘靖迪 <liujingdi@liujingdideMacBook-Pro.local>
2017-12-13 10:52:02 +0800

    Initial Commit

---
+++ 3328/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  3328
+//
+//  Created by 刘靖迪 on 2017/12/13.
+//  Copyright © 2017年 刘靖迪. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
